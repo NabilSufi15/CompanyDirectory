8f33ed729dcb25de9585513aa4dd9bb164ff174c
NabilSufi15 <nabilsufi15@gmail.com>
2021-01-31 18:43:20 +0000

    changed config file

--- libs/php/config.php
+++ libs/php/config.php
@@ -1,10 +1,18 @@
 <?php
+	//Development Connection
+	// $cd_host = "127.0.0.1";
+	// $cd_port = 3306;
+	// $cd_socket = "";
+	// $cd_user = "root"; // user name
+	// $cd_password = ""; // password
+	// $cd_dbname = "company_directory"; // database name
 
-	$cd_host = "127.0.0.1";
+	//Remote Database Connection
+	$cd_host = "sql2.freesqldatabase.com";
 	$cd_port = 3306;
 	$cd_socket = "";
-	$cd_user = "root"; // user name
-	$cd_password = ""; // password
-	$cd_dbname = "company_directory"; // database name
+	$cd_user = "sql2389955"; // user name
+	$cd_password = "vK8*cW5!"; // password
+	$cd_dbname = "sql2389955"; // database name
 
 ?>
