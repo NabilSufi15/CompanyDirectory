859650aa141abd6aaab65a1f3d099136fbf1a6ab
NabilSufi15 <nabilsufi15@gmail.com>
2021-02-16 16:52:17 +0000

    add favicon

--- libs/php/config.php
+++ libs/php/config.php
@@ -1,18 +1,18 @@
 <?php
 	//Development Connection
-	// $cd_host = "127.0.0.1";
-	// $cd_port = 3306;
-	// $cd_socket = "";
-	// $cd_user = "root"; // user name
-	// $cd_password = ""; // password
-	// $cd_dbname = "company_directory"; // database name
-
-	//Remote Connection
-	$cd_host = "eu-cdbr-west-03.cleardb.net";
+	$cd_host = "127.0.0.1";
 	$cd_port = 3306;
 	$cd_socket = "";
-	$cd_user = "bc5d651f8768a5"; // user name
-	$cd_password = "131b8cf2"; // password
-	$cd_dbname = "heroku_ef04479932e4f02"; // database name
+	$cd_user = "root"; // user name
+	$cd_password = ""; // password
+	$cd_dbname = "company_directory"; // database name
+
+	//Remote Connection
+	// $cd_host = "eu-cdbr-west-03.cleardb.net";
+	// $cd_port = 3306;
+	// $cd_socket = "";
+	// $cd_user = "bc5d651f8768a5"; // user name
+	// $cd_password = "131b8cf2"; // password
+	// $cd_dbname = "heroku_ef04479932e4f02"; // database name
 
 ?>
